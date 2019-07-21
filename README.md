95666adcacf4ffb8f59546bfc932dae9ece107f9
Felix Volny <github@volny.co>
2019-07-21 16:38:36 +0700

    ready

---
+++ README.md
@@ -0,0 +1,17 @@
+# Notational - A utility for creating and opening Notational files
+
+Takes a note title as argument, creates a file of `<timestamp>-<title>.md`, and opens the file in your favorite editor.
+
+If you don't specify the editor with the `--editor` flag your `EDITOR` environment variable will be used.
+
+> Usage: notational [options] [title]
+> 
+> Options:
+>   -e, --editor [command]  choose editor
+>   -V, --version           output the version number
+>   -h, --help              output usage information
+> 
+> Examples:
+>   $ notational "My Note Title"
+>   $ notational --editor code "My Note Title"
+> 
--- notational.js
+++ notational.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 
-// TODO
-// allow for string as title - uppercase to title, `-`-case in fileName (just use lodash)
-// format date in template
-// upload to homebrew to @volny namespace - to be executed with `notational`
-// read an env var `$NOTATIONAL_EDITOR` to permanently set yours
-
 const program = require('commander')
 const fs = require('fs')
 const { spawn } = require('child_process')
+const kebabCase = require('lodash.kebabcase')
 
 program
   .option('-e, --editor [command]', 'choose editor')
@@ -18,8 +13,8 @@ program
 program.on('--help', () => {
   console.info('')
   console.info('Examples:')
-  console.info('  $ notational my-note-title')
-  console.info('  $ notational --editor code my-note-title')
+  console.info('  $ notational "My Note Title"')
+  console.info('  $ notational --editor code "My Note Title"')
 })
 
 program.parse(process.argv)
@@ -28,7 +23,7 @@ const [title] = program.args
 
 const makeFileName = title => {
   if (typeof title === 'string') {
-    return `${+new Date()}-${title}.md`
+    return `${+new Date()}-${kebabCase(title)}.md`
   }
   return `${+new Date()}.md`
 }
@@ -38,14 +33,13 @@ const fileName = makeFileName(title)
 const onCreateSuccess = () => {
   // open in editor
   const editor = program.editor || process.env.EDITOR
+
   // focus end of file if vim
-  const editorCommand = ['vim', 'nvim'].includes(editor)
-    ? `${editor} -c "$"`
-    : editor
+  const extraArgs = ['vim', 'nvim'].includes(editor)
+    ? `-c $`
+    : ''
 
-  console.log({ editorCommand })
-
-  const child = spawn(editorCommand, [`./${fileName}`], {
+  const child = spawn(editor, [extraArgs, `./${fileName}`], {
     // https://stackoverflow.com/questions/9122282/how-do-i-open-a-terminal-application-from-node-js
     stdio: 'inherit',
   })
@@ -53,10 +47,11 @@ const onCreateSuccess = () => {
   child.on('exit', function(e, code) {
     process.exit()
   })
+
 }
 
 const template = `---
-title: ${title}
+title: ${title || ''}
 date: ${new Date()}
 ---
 
